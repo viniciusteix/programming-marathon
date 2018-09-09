6cd0b5d5faaf819b1edda5dda38a984a25352549
Vinicius Teixeira <teixeira.ufc@gmail.com>
2018-09-09 00:37:31 -0300

    algorithms

---
+++ algorithms/coin_change.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+  
+int count( int S[], int m, int n ) { 
+    int table[n+1]; 
+  
+    memset(table, 0, sizeof(table)); 
+  
+    table[0] = 1; 
+  
+    for(int i=0; i<m; i++) 
+        for(int j=S[i]; j<=n; j++) 
+            table[j] += table[j-S[i]]; 
+  
+    return table[n]; 
+} 
+  
+int main() { 
+    int arr[] = {1, 2, 3}; 
+    int m = sizeof(arr)/sizeof(arr[0]); 
+    int n = 4; 
+    printf("%d\n", count(arr, m, n)); 
+    return 0; 
+} 
